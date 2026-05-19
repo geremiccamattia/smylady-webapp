4e97675baac772ea052d278d0bed2786be0c674b
geremiccamattia <geremicca.mattia@gmail.com>
2026-05-19 22:27:11 +0200

    fix: added favicons

--- smylady-web/src/app/layout.tsx
+++ smylady-web/src/app/layout.tsx
@@ -16,6 +16,18 @@ export const metadata: Metadata = {
     images: [{ url: 'https://shareyourparty.de/logo.png' }],
   },
   metadataBase: new URL('https://shareyourparty.de'),
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: { url: '/apple-touch-icon.png', sizes: '180x180' },
+    other: [
+      { rel: 'icon', url: '/favicon-192x192.png', sizes: '192x192' },
+      { rel: 'icon', url: '/favicon-512x512.png', sizes: '512x512' },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
