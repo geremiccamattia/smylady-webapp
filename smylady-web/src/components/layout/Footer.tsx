660801d230627ff0a079417b0324471309517938
geremiccamattia <geremicca.mattia@gmail.com>
2026-06-08 21:34:24 +0200

    feat: added blog link in footer

--- smylady-web/src/components/layout/Footer.tsx
+++ smylady-web/src/components/layout/Footer.tsx
@@ -78,6 +78,7 @@ export default function Footer() {
               <li><Link href={localePath('/veranstalter')} className="hover:text-foreground">{t('footer.organizerInfo')}</Link></li>
               <li><Link href={localePath('/events/wien')} className="hover:text-foreground">Events in Wien</Link></li>
               <li><Link href={localePath('/grounding')} className="hover:text-foreground">Über Share Your Party</Link></li>
+              <li><a href="https://blog.shareyourparty.de" target="_blank" rel="noopener noreferrer" className="hover:text-foreground">Blog</a></li>
             </ul>
           </div>
 
